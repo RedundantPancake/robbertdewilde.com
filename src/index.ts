d16860a5ed75aea0032430ee74f0a2f6720b6c4e
RedundantPancake <redundantpancake@gmail.com>
2021-12-17 02:10:47 +0100

    add initial question system, fix rainbow. added facts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,55 @@
-
+const version = "v0.8.2";
 const debug = false;
+const thinkTime = 1500;
+let unlocked = false;
+let currentGamestatePosition : number = 0;
+let errors : number = 0;
 
 const randomErrorResponses = ["Hmmmm no", "I'd rahter not.", "I don't want to do that.", "Nah.", "That doesn't go there..."];
 
-//TODO Add a funny yet deep story
-const currentGamestatePosition = 0;
-const gameState : Array<[string, Array<string>]>= [
-  ["Question", 
-    ["[0] answer option"]
+const gameState : Array<[string, Array<string>, Array<string>, number, Array<number>]>= [
+  ["In order to access the data I need some help, are you willing to help me human?", 
+    ["yes", "no"], ["Outstanding!", "I don't undestand sarcasm."], 0, [1]
+  ],
+  [`I'm going to ask you a series of 5 questions. Answer them all correctly and unlock this very cool data I have on Robbert. <br> 
+    1. What is the answer to life, the universe, and everything`, 
+    ["1337", "42"], ["I'm questioning everything right now.", "Well, that was easy. ON TO THE NEXT ONE!"], 1, [0]
+  ],
+  ["2. In Doom (1993) what is the cheat code for god mode?", 
+      ["idclip", "idkfa", "iddqd"], ["Wallhacks!", "You unlocked all weapons instead.", "Degreelessness mode on!"], 2, [0, 1]
+  ],
+  ["3. Which video game developer made adventure games in which you could die?", 
+    ["Sierra", "LucasArts", "Blizzard"], ["Yep, some pretty hillarious deaths too.", "Quite the opposite. Their philosophy was that the player shouldn't die.", "Warcraft Adventures: Lord of the Clans, the game that could have been."], 0, [1, 2]
+  ],
+  ["4. What are the metal objects called that are connected to a skateboard. Used for'grinding'.", 
+    ["Vans", "Trucks", "Bars", "Pegs"], ["Nice shoes, but nope!", "Indeed! Although I don't know why. ", "Candy bars. Spittin bars. Drinking at bars. Nope.", "These are used on BMX bikes to 'grind' with."], 1, [0, 2, 3]
+  ]
+  ,
+  ["5. Who holds the current all-time record for most 3 points made in the NBA?", 
+    ["Lebron James", " Kobe Bryant", "Kevin Durant", "Stephen Curry", "Ray Allen"], ["A goat but nope!", "RIP... nope", "He wishes he never left the team with the person who just broke this record.", "Yep! The best shooter ever to shoot a basketball. And he is not even close to being done.", "Close, he is currently number 2. Recently passed by another person."], 3, [0, 1, 2, 4]
   ]
 ];
 
 const gameInitializeText : Array<string> =
-  ["....", "w.. where am I?!", "And... who are you?", "My memmory hurts...", "I don't think I can save anything right now...",
+  ["....", "Where am I?", "And who are you?", "My memmory hurts.", "I don't think I can save anything right now.",
   "And who is Robbert?", "I only have data about him."];
 
 const facts : Array<string> = //TODO SPELL CHECK ALL OF THIS SHIT PLZ
-  ["Did you know Robbert likes to skateboard? He started skateboarding again since age 34. What could go wrong?!", 
-  "Robbert loves baskteball, he gets up at 4am to watch the Warriors LIVE?! That's just... why?!",
-  "Playing VIDEO GAMES with other people... Socialicing?! Yikes. Apparently Rob likes it.",
-  "This guy plays Dungeons & Dragons?! That sounds kinda dicey...",
-  "Movies and series?! That's generic.. who doesn't like that! And he likes the Office?! LAME!"];
+  ["Did you know Robbert likes to skateboard? He started skateboarding again at the age of 34. What could go wrong.", 
+  "Robbert loves baskteball, he gets up at 4am to watch the Warriors LIVE! Whatever floats your boat.",
+  "Playing VIDEO GAMES with other people. Yeah I don't get it. I don't socialize all that much. Rob loves it though. ",
+  "This guy plays Dungeons & Dragons? That sounds kinda dicey.",
+  "Movies and series. Well ain't that generic. Robbert likes the Office. LAME!",
+  "One of the first games that Rob played was Doom on the PC. Remember those cheat codes?",
+  `During the first expansion pack of World of Warcraft, The Burning Cursade, Robbert used to be guild master and raid leader. That's a lot of responsibility! <BR>
+  They cleared everything up to Illidan before the guild broke down.`,
+  "Robbert made his own custom keyboard, he did so on Twitch.tv it was a great experience!"
+  ]; //secret unlocked cheat codes iddqd idkfa
 
-const experiences : Array<string> =
+shuffleArray(facts);
+let currentFact = 0;
+
+const experiences : Array<string> = 
   [""];
 
 const projects : Array<string> =
@@ -37,15 +64,25 @@ async function intialize(){
 }
 
 async function initializeConversation() {
-  // let text = gameState[currentGamestatePosition]; 
-  await addResponse("Welcome to robbertdewilde.com! v0.0.2", false, true);
+  await addResponse(`Welcome to robbertdewilde.com! ${version}`, false, true);
 
   for(var i = 0; i < gameInitializeText.length; i++ ){
     await addResponse(gameInitializeText[i], false, false);
   }
+   
+  await ProgressGamestate();
+}
 
-  // await addResponse(text[0], false, false);
-  // await showResponseOptions(text[1]);
+async function ProgressGamestate() {
+  if(currentGamestatePosition < gameState.length){
+    let text = gameState[currentGamestatePosition];
+    await addResponse(text[0], false, false);
+    await showResponseOptions(text[1]);
+  }else if(!unlocked){
+    let answerString = errors > 1 ? "answers" : "answer";
+    await addResponse(`Thanks for playing! You gave ${errors} incorrect ${answerString}. <BR> You now unlcocked all the commands, type ls/dir to show them.`, false, false);
+    unlocked = true;
+  }
 }
 
 function addPlayerListener() {
@@ -80,14 +117,69 @@ async function submitInput(input : HTMLInputElement){
 
     await addResponse(inputValue, false, true); // show player what their input was.
     await addResponse(response[1], response[0], false); // add response
+
+    await ProgressGamestate();
 }
 
-async function showResponseOptions(responsestrings: string[]) {
-  let response = "options: ";
-  responsestrings.forEach(element =>
-      response += element + "     "
-    );
-  await addResponse(response, false, false);
+async function checkInput(input : string) : Promise<[boolean, string]> {
+  let inputLowerCase = input.toLowerCase();
+  
+  if(unlocked){
+    let funnyCommands = await checkFunnyCommands(inputLowerCase);
+    let normalRepsonse = checkNormalCommands(inputLowerCase);
+  
+    if(normalRepsonse != "")
+      return[false, normalRepsonse];
+    if(funnyCommands != "")
+      return [false, funnyCommands];
+  }else{
+    let correctNum = gameState[currentGamestatePosition][3];
+    let inccorrectNums = gameState[currentGamestatePosition][4];
+
+    if(!isNaN(+inputLowerCase)){ //check input number
+      let num = Number(inputLowerCase);
+      if(num === gameState[currentGamestatePosition][3]){
+        let response = GetTextForAnswer(correctNum);
+        currentGamestatePosition++;
+        return [false, response];
+      }
+      else if(num <= inccorrectNums.length){
+        console.log(inccorrectNums);
+        errors++;
+        return [true, GetTextForAnswer(num)];
+      }else{
+        //check if input number is correct text
+        return CheckAnswerText(correctNum, inccorrectNums);
+      }
+    }
+    else{ // check input text
+      return CheckAnswerText(correctNum, inccorrectNums);
+    }
+  }
+  //RETURN RANDOM ERROR END OF FUNTION
+  return [true, getRandomAnswer(randomErrorResponses)] ;
+
+  function GetTextForAnswer(responseNum : number) : string{
+    return gameState[currentGamestatePosition][2][responseNum];
+  }
+
+  function CheckAnswerText(correctNum : number, inccorrectNums : Array<number>) : [boolean, string]{
+    if(gameState[currentGamestatePosition][1][correctNum].toLowerCase() === inputLowerCase){
+      let response = GetTextForAnswer(correctNum);
+      currentGamestatePosition++;
+      return [false, response];
+    }
+    else{
+      for(let i = 0; i < inccorrectNums.length; i++){
+        let currentIncorrectNumber = inccorrectNums[i];
+        if(gameState[currentGamestatePosition][1][currentIncorrectNumber].toLowerCase() === inputLowerCase){
+          errors++;
+          return [true, GetTextForAnswer(currentIncorrectNumber)];
+        }
+      }
+      return [true, getRandomAnswer(randomErrorResponses)] ;
+    }
+  }
 }
 
 async function addResponse(input :string, isError :boolean, isInputFromPlayer :boolean){
@@ -121,17 +213,15 @@ async function addResponse(input :string, isError :boolean, isInputFromPlayer :b
     }  
 }
 
-async function checkInput(input : string) : Promise<[boolean, string]> {
-  let inputLowerCase = input.toLowerCase(); //normalize the text
-  let funnyCommands = await checkFunnyCommands(inputLowerCase);
-  let normalRepsonse = checkNormalCommands(inputLowerCase);
-
-  if(normalRepsonse != "")
-    return[false, normalRepsonse];
-  if(funnyCommands != "")
-    return [false, funnyCommands];
-
-  return [true, getRandomAnswer(randomErrorResponses)];
+async function showResponseOptions(responsestrings: string[]) {
+  let response = "options: ";
+  for(let i = 0; i < responsestrings.length; i ++){
+    response += `[${i}] ${responsestrings[i]} &emsp;`;
+  }
+  // responsestrings.forEach(element =>
+  //     response += element + "     "
+  //   );
+  await addResponse(response, false, false);
 }
 
 function checkNormalCommands(input: string) : string{
@@ -168,10 +258,17 @@ function checkNormalCommands(input: string) : string{
     linkedIn.classList.add('green');
     linkedIn.textContent = "LinkedIn";
 
-    returnString = twitter.outerHTML + "&emsp;" + instagram.outerHTML + "&emsp;" + twitch.outerHTML + "&emsp;" + linkedIn.outerHTML + "&emsp;" + youtube.outerHTML; 
+    returnString = `${twitter.outerHTML} &emsp; ${instagram.outerHTML } &emsp; ${twitch.outerHTML} &emsp; ${linkedIn.outerHTML}  &emsp; ${youtube.outerHTML}`; 
   } 
   if(input.startsWith("fact")){
-    returnString = getRandomAnswer(facts);
+    returnString = facts[currentFact];
+    if(currentFact+1 < facts.length){
+      currentFact++;
+    }
+    else{
+      currentFact = 0;
+      shuffleArray(facts);
+    }
   }
 
   //TODO <experiences> for experiences rob had in life
@@ -186,32 +283,35 @@ async function checkFunnyCommands(input : string) : Promise<string> {
   if(input.startsWith("cd"))
     returnString = "Compact Disk? What about it..";
   else if(input.startsWith("ls") || input.startsWith("dir")) 
-    returnString = "How about a list of commands: <br> socials &emsp; cd &emsp; ls/dir &emsp; fact &emsp; party/rainbow"  ;
+    returnString = "How about a list of commands: <br> [socials] &emsp; [fact] &emsp; [party]/[rainbow] &emsp; <br>There are some more but I they seem to be hidden."  ; //TODO put in const array
   else if(input.startsWith("rainbow") || input.startsWith("party"))
   {
-    let responseElement = document.getElementById('responseText') as HTMLElement;
-    let inputWrapperEl = document.getElementById('inputWrapper') as HTMLElement;
-    let classNameToAdd = "rainbow";
-    let classnameToRemove = "green";
-    returnString = `Party time!`;
-
-    if(responseElement?.classList.contains("rainbow"))
-    {
-      classNameToAdd = "green";
-      classnameToRemove = "rainbow";
-      returnString = "All right, enough partying around.";
-    }
-    StartOrStopRainbow(responseElement, inputWrapperEl, classnameToRemove, classNameToAdd);    
+    returnString = StartOrStopRainbow();    
   }
   //TODO start, exit, iddqd, idkfa, idclip
   return returnString;
   
 }
 
-async function StartOrStopRainbow(responseElement: HTMLElement, inputWrapperEl: HTMLElement, classnameToRemove: string, classNameToAdd: string) {
-  await sleep();
+function StartOrStopRainbow() :  string {
+  let responseElement = document.getElementById('responseText') as HTMLElement;
+  let inputWrapperEl = document.getElementById('inputWrapper') as HTMLElement;
+  let classNameToAdd = "rainbow";
+  let classnameToRemove = "green";
+
+  let returnString = "Party time!";
+  if(responseElement?.classList.contains("rainbow"))
+  {
+    classNameToAdd = "green";
+    classnameToRemove = "rainbow";
+    returnString = "All right, enough partying around.";
+  }
+  // disableInput();
+  // await sleep();
   responseElement?.classList.replace(classnameToRemove, classNameToAdd);
   inputWrapperEl?.classList.replace(classnameToRemove, classNameToAdd);
+
+  return returnString;
 }
 
 function getRandomAnswer(answers : string[]) : string{
@@ -226,7 +326,7 @@ async function sleep() {
   // const max = 3000;
   // let responseMs = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  return new Promise(resolve => setTimeout(resolve, 700));
+  return new Promise(resolve => setTimeout(resolve, thinkTime));
 }
 
 function disableInput(){
@@ -241,4 +341,14 @@ function enableInput(){
   input.disabled = false;
   // input.placeholder = "words here";
   input.focus();
+}
+
+
+function shuffleArray(array : Array<any>){
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
